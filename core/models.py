194f61dad21b06ba5c7df97967d54db65da3d257
Haditech2 <your-email@example.com>
2025-12-11 18:12:20 +0100

    Store images as base64 in PostgreSQL database

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import base64
 
 # Create your models here.
 
@@ -9,10 +10,30 @@ class Executive(models.Model):
     phone_number = models.CharField(max_length=20, blank=True)
     bio = models.TextField(blank=True)
     photo = models.ImageField(upload_to='executives/', blank=True)
+    photo_data = models.TextField(blank=True, help_text="Base64 encoded image for database storage")
     order = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.name
+    
+    def get_photo_url(self):
+        """Return photo URL or data URI for database-stored images"""
+        if self.photo_data:
+            return f"data:image/jpeg;base64,{self.photo_data}"
+        elif self.photo:
+            return self.photo.url
+        return None
+    
+    def save(self, *args, **kwargs):
+        # If photo file is uploaded, convert to base64 and store in photo_data
+        if self.photo:
+            try:
+                self.photo.seek(0)
+                image_data = self.photo.read()
+                self.photo_data = base64.b64encode(image_data).decode('utf-8')
+            except:
+                pass
+        super().save(*args, **kwargs)
 
     class Meta:
         ordering = ['order']

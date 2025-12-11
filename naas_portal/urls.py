42f782f9aa7c20548e4d7bc870ee4bac0bc176e6
Haditech2 <your-email@example.com>
2025-12-11 17:52:33 +0100

    Fix favicon.ico 404 error - add redirect to logo

--- naas_portal/urls.py
+++ naas_portal/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url='/static/images/logo.png', permanent=True)),
     path('', include('core.urls')),
     path('news/', include('news.urls')),
     path('events/', include('events.urls')),
@@ -31,3 +33,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
